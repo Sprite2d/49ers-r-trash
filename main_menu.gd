4ac305aa7ae76cd5d3d625fea82e56e21f7647e3
49erssuck <157193582+49erssuck@users.noreply.github.com>
2024-01-21 10:41:54 +0530

    14th comit
    we have completed making level 3

--- main_menu.gd
+++ main_menu.gd
@@ -11,3 +11,7 @@ func _on_level_1_pressed():
 
 func _on_level_2_pressed():
 	get_tree().change_scene_to_file("res://level 2.tscn")
+
+
+func _on_level_3_pressed():
+	get_tree().change_scene_to_file("res://level 3.tscn")
--- quetion_2.gd
+++ quetion_2.gd
@@ -3,8 +3,8 @@ extends Node
 
 
 func _on_ans_1_pressed():
-	get_tree().change_scene_to_file("res://level 2.tscn")
+	get_tree().change_scene_to_file("res://level 3.tscn")
 
 
 func _on_ans_2_pressed():
-	get_tree().change_scene_to_file("res://main_menu.tscn")
+	get_tree().change_scene_to_file("res://level 2.tscn")
