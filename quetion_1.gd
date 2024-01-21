3108e3ad5835a68b93174a605ba76988760bfdec
49erssuck <157193582+49erssuck@users.noreply.github.com>
2024-01-21 07:38:12 +0530

    13th comit
    we have made our math problems for our levels to progress to next level we have to answer correctly

---
+++ quetion_1.gd
@@ -0,0 +1,15 @@
+extends Node
+
+
+
+
+func _on_ans_1_pressed():
+	get_tree().change_scene_to_file("res://level 2.tscn")
+	
+	
+	
+
+
+
+func _on_ans_2_pressed():
+	get_tree().change_scene_to_file("res://level 1.tscn")
---
+++ quetion_2.gd
@@ -0,0 +1,10 @@
+extends Node
+
+
+
+func _on_ans_1_pressed():
+	get_tree().change_scene_to_file("res://level 2.tscn")
+
+
+func _on_ans_2_pressed():
+	get_tree().change_scene_to_file("res://main_menu.tscn")
